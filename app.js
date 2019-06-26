c8ba3833b9460dd3508789aee255837528f0d981
WXXJXN <poddle93@gmail.com>
2019-06-26 18:03:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
   console.log('server started!!!')
   res.writeHead(200, { 'Content-Type' : 'text/plain' });
-  res.end('Hello World KING tq');
+  res.end('Hello World KING 찐막');
 });
 
 server.listen(8081);
